168f19c86001675ac60f3c0fbc8a47f30abab06c
Yayadu665 <61546830+Yayadu665@users.noreply.github.com>
2020-02-28 07:01:05 +0100

    Update and rename scatter-silo.js to shop.js

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,4 +1,4 @@
-const silo = extendContent(Block, "scatter-silo", {
+const silo = extendContent(Block, "shop", {
     buildConfiguration(tile, table){
 		//Copper contre 2 tickets
         table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
