1b57d49caf0cd93ea4b6921258b855e42342c050
Yayadu665 <61546830+Yayadu665@users.noreply.github.com>
2020-09-11 19:56:48 +0200

    Update shop.js

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,40 +1,37 @@
 const shop = extendContent(Block, "shop", {
-    buildConfiguration(tile, table){
+	buildConfiguration(tile, table){
 		//Copper contre 2 tickets
-        table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 2, Items.copper);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
+			convertitem(this, tile, 2, Items.copper);
+		})).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
 		//Lead contre 4 tickets
 		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 4, Items.lead);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+			convertitem(this, tile, 4, Items.lead);
+		})).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
 		//Titanium contre 10 tickets
 		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 10, Items.titanium);
-	    Font font = Fonts.outline;
-	    CharSequence text = "couocu";
-	    font.draw(text)
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+			convertitem(this, tile, 10, Items.titanium);
+		})).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
 		//Coal contre 6 tickets
 		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 6, Items.coal);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+			convertitem(this, tile, 6, Items.coal);
+		})).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
 		//Sand contre 6 tickets
 		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 6, Items.sand);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+			convertitem(this, tile, 6, Items.sand);
+		})).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
 		//Graphite contre 12 tickets
 		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 12	, Items.graphite);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
-    },
+			convertitem(this, tile, 12	, Items.graphite);
+		})).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+	},
 })
 
 function convertitem(main, tile, value, item){
 	if(tile.entity.items.total()>=value){
-			const ticket = Vars.content.getByName(ContentType.item, "addmod-ticket");
-			tile.entity.items.add(item, 1);
-			main.tryDump(tile, item);
-            tile.entity.items.remove(ticket,value)
+		const ticket = Vars.content.getByName(ContentType.item, "addmod-ticket");
+		tile.entity.items.add(item, 1);
+		main.tryDump(tile, item);
+		tile.entity.items.remove(ticket,value)
         }
 }
