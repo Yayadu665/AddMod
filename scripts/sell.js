8b6e176b699c95f253aac273357a5879a5f4abda
Yayadu665 <61546830+Yayadu665@users.noreply.github.com>
2020-09-11 19:23:36 +0200

    Update sell.js

--- scripts/sell.js
+++ scripts/sell.js
@@ -1,29 +1,6 @@
 const silo = extendContent(Block, "sell", {
     buildConfiguration(tile, table){
-		//Copper contre 2 tickets
-        table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 2, Items.copper);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
-		//Lead contre 4 tickets
-		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 4, Items.lead);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
-		//Titanium contre 10 tickets
-		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 10, Items.titanium);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
-		//Coal contre 6 tickets
-		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 6, Items.coal);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
-		//Sand contre 6 tickets
-		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 6, Items.sand);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
-		//Graphite contre 12 tickets
-		table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            convertitem(this, tile, 12	, Items.graphite);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+	//Code
     },
 })
 
