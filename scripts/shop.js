ceb5db1b668a933804bd86bc203fc56ee402f7f4
Yayadu665 <61546830+Yayadu665@users.noreply.github.com>
2020-09-12 22:46:38 +0200

    Update shop.js

--- scripts/shop.js
+++ scripts/shop.js
@@ -67,7 +67,6 @@ const calamity = extendContent(LaserTurret, "armagedon",{
 		Draw.rect(Core.atlas.find("addmod-block-7"), tile.drawx(), tile.drawy())
 	}
 });
-calamity.shootType = meltdownLaser;
 calamity.update = true;
 
 const shop = extendContent(Block, "shop", {
