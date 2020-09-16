8de090c41f53d93eca1580cdb91de0c54298b02f
Yayadu665 <61546830+Yayadu665@users.noreply.github.com>
2020-09-16 20:55:59 +0200

    Update shop.js

--- scripts/shop.js
+++ scripts/shop.js
@@ -58,12 +58,12 @@ calamitylaser.smokeEffect = Fx.none;
 const calamity = extendContent(LaserTurret, "armagedon",{
 	generateIcons: function(){
 	return [
-		Core.atlas.find("addmod-block-7"),
-		Core.atlas.find("addmod-armagedon")
+		Core.atlas.find("aetherunbound-block-7"),
+		Core.atlas.find("aetherunbound-armagedon")
 	];},
 	
 	draw: function(tile){
-		Draw.rect(Core.atlas.find("addmod-block-7"), tile.drawx(), tile.drawy())
+		Draw.rect(Core.atlas.find("aetherunbound-block-7"), tile.drawx(), tile.drawy())
 	}
 });
 calamity.shootType = calamitylaser;
